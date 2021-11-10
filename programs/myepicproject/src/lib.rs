bd6da3c50e80ea6dd94efa4426853f3bdcaac1c5
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-11-09 22:55:08 -0500

    initialize project with anchor, get test passing

---
+++ programs/myepicproject/src/lib.rs
@@ -0,0 +1,14 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod myepicproject {
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+        Ok(());
+    }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff {}

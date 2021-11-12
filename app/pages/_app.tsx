2f7d03326eaaddbb8ae89d010ae4276e0106e997
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-11-11 22:37:33 -0500

    setup app with nextjs and typescript

---
+++ app/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+declare global {
+  interface Window {
+    solana: any
+  }
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+export default MyApp
---
+++ app/utils/constants.ts
@@ -0,0 +1,2 @@
+export const TWITTER_HANDLE = 'polkamerc'
+export const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
